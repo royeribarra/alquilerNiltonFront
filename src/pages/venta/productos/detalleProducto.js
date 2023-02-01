96ff0345aead3c179e0698abeefb7de8dcd48d9b
royeribarra <royeribarrao@gmail.com>
2023-02-01 01:07:34 -0500

    modulo de venta

--- src/pages/venta/productos/detalleProducto.js
+++ src/pages/venta/productos/detalleProducto.js
@@ -11,7 +11,7 @@ import {
   Button
 } from "antd";
 import { NavLink } from "react-router-dom";
-import "./listaProductos.css";
+import "./detalleProducto.css";
 
 const { Option } = Select;
 
@@ -41,34 +41,35 @@ function DetalleProducto()
 
   
   return(
-    <Card title="Detalle producto">
-      <Form layout="vertical">
+    <Form layout="vertical">
+    <Card title="Detalle producto" className="card-detalle-producto">
+      
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" xs={24} md={24}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={24}>
             <Form.Item
               label="Descripción extra"
               name="descripcionExtra"
             >
-              <Input />
+              <Input className="form-control-detalle" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Marca"
               name="marca"
             >
-              <Input placeholder="Ejm: Lopez Clemente" />
+              <Input className="form-control-detalle" placeholder="Ejm: Lopez Clemente" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Tela"
               name="tela"
             >
-              <Input placeholder="Email" />
+              <Input className="form-control-detalle" placeholder="Email" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Tamaño"
               name="tamano"
@@ -76,15 +77,15 @@ function DetalleProducto()
               <InputNumber className="input-numerico" placeholder="Ejm: 934448755" minLength={9} maxLength={9} />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Tonalidad"
               name="montoPagado"
             >
-              <Input placeholder="Ejm: Elenco Santísima" />
+              <Input className="form-control-detalle" placeholder="Ejm: Elenco Santísima" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Modelo {tela}"
               name="tipoPago"
@@ -97,33 +98,34 @@ function DetalleProducto()
               />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={8}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={8}>
             <Form.Item
               label="Color"
               name="tipoPago"
             >
-              <Input />
+              <Input className="form-control-detalle" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={12}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={12}>
             <Form.Item
               label="Ocasión"
               name="tipoPago"
             >
-              <Input />
+              <Input className="form-control-detalle" />
             </Form.Item>
           </Col>
-          <Col className="gutter-row" xs={24} md={12}>
+          <Col className="gutter-row item-form-detalle" xs={24} md={12}>
             <Form.Item
               label="Aroma"
               name="tipoPago"
             >
-              <Input />
+              <Input className="form-control-detalle" />
             </Form.Item>
           </Col>
         </Row>
-      </Form>
+      
     </Card>
+    </Form>
   );
 }
 
